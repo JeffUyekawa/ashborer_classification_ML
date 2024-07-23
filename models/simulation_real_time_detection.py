e24c3d15ec598ea8850a14cf82b7f83deb33744f
Jeff Uyekawa <jeffuyekawa@gmail.com>
2024-07-23 13:26:55 -0700

    updated

--- models/simulation_real_time_detection.py
+++ models/simulation_real_time_detection.py
@@ -15,6 +15,53 @@ CHANNELS = 1
 RATE = 48000
 CHUNK = int(RATE * 0.05) 
 
+class CNNNetwork(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv1=nn.Sequential(
+            nn.Conv2d(in_channels=1,out_channels=16,kernel_size=3,stride=1,padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv2=nn.Sequential(
+            nn.Conv2d(in_channels=16,out_channels=32,kernel_size=3,stride=1,padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv3=nn.Sequential(
+            nn.Conv2d(in_channels=32,out_channels=64,kernel_size=3,stride=1,padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv4=nn.Sequential(
+            nn.Conv2d(in_channels=64,out_channels=128,kernel_size=3,stride=1,padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv5=nn.Sequential(
+            nn.Conv2d(in_channels=128,out_channels=256,kernel_size=3,stride=1,padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.flatten=nn.Flatten()
+        self.linear1=nn.Linear(in_features=1024,out_features=128)
+        self.linear2=nn.Linear(in_features=128,out_features=1)
+        self.output=nn.Sigmoid()
+    
+    def forward(self,input_data):
+        x=self.conv1(input_data)
+        x=self.conv2(x)
+        x=self.conv3(x)
+        x=self.conv4(x)
+        x=self.conv5(x)
+        x=self.flatten(x)
+        x=self.linear1(x)
+        logits=self.linear2(x)
+        output=self.output(logits)
+        
+        return output
+
 def bandpass_filter(data,fs, lowcut=1000, highcut=12000, order=5, pad = 0):
     nyquist = 0.5 * fs
     low = lowcut / nyquist
@@ -28,14 +75,14 @@ def chunk_to_spec(audio_chunk):
     y = transform(audio_chunk)
     return y
 
-def classify_chunk(audio_chunk):
+def classify_chunk(model,audio_chunk):
     spec = chunk_to_spec(audio_chunk)
     with torch.no_grad():
-        output = model1(spec)
+        output = model(spec)
         pred =  (output>=0.5)*1
     return pred.item()
 
-def simulate_real_time_classification(wav_file, duration = 30):
+def simulate_real_time_classification(model,wav_file, duration = 30):
     audio_data, sr = ta.load(wav_file)
     y,fs = sf.read(wav_file)
     sd.play(y,fs)
@@ -61,7 +108,7 @@ def simulate_real_time_classification(wav_file, duration = 30):
             audio_chunk = bandpass_filter(audio_chunk,sr)
             audio_chunk = torch.from_numpy(audio_chunk.astype('f'))
             audio_chunk = audio_chunk.unsqueeze(0)
-            pred = classify_chunk(audio_chunk)
+            pred = classify_chunk(model,audio_chunk)
             
             if pred== 1:
                 print(f'Event Detected at {int(start_idx/sr)} seconds')
@@ -75,9 +122,9 @@ def simulate_real_time_classification(wav_file, duration = 30):
 
 if __name__ == "__main__":
     
-    model = torch.load(r"C:\Users\jeffu\OneDrive\Documents\Jeff's Math\Ash Borer Project\models\saved_2D_model.pt")
-    wav_file = r"C:\Users\jeffu\OneDrive\Documents\Jeff's Math\Ash Borer Project\Datasets\validation_recordings - Copy\Clip 3.wav"
-    simulate_real_time_classification(wav_file, duration=30)
+    model = torch.load(r"C:\Users\jeffu\OneDrive\Documents\Jeff's Math\Ash Borer Project\models\saved_2D_model.pth")
+    wav_file = r"C:\Users\jeffu\OneDrive\Documents\Jeff's Math\Ash Borer Project\Datasets\validation_recordings - Copy\Clip 4.wav"
+    simulate_real_time_classification(model,wav_file, duration=30)
 
 
 
