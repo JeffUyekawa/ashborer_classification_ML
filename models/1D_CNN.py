0f95a9c79b1ea067fc885cad1560d3d1563be3f0
Jeff Uyekawa <jeffuyekawa@gmail.com>
2024-09-04 11:25:37 -0700

    added label smoothing, cleaned up

--- models/1D_CNN.py
+++ models/1D_CNN.py
@@ -13,134 +13,14 @@ import sys
 from time import time
 from sklearn.metrics import log_loss, accuracy_score
 
+
 #sys.path.insert(1, "/home/jru34/Ashborer/Models")# For Monsoon
-sys.path.insert(1, r"C:\Users\jeffu\OneDrive\Documents\Jeff's Math\Ash Borer Project\pre_processing")
-from custom_dataset_class import borer_data
-#%%
-
-if torch.cuda.is_available():
-    device='cuda'
-else:
-    device='cpu'
-
-TRAIN_ANNOTATION = r"C:\Users\jeffu\OneDrive\Documents\Jeff's Math\Ash Borer Project\Datasets\training_data_info.csv"
-VAL_ANNOTATION = r"C:\Users\jeffu\OneDrive\Documents\Jeff's Math\Ash Borer Project\Datasets\val_data_info.csv"
-
-TRAIN_AUDIO = r"C:\Users\jeffu\Documents\Recordings\recordings_for_train"
-VAL_AUDIO = r"C:\Users\jeffu\Documents\Recordings\recordings_for_test"
-
-'''
-TRAIN_ANNOTATION = "/home/jru34/Ashborer/Datasets/training_data_info.csv"
-VAL_ANNOTATION = "/home/jru34/Ashborer/Datasets/val_data_info.csv"
-
-TRAIN_AUDIO = "/home/jru34/Ashborer/Audio_Files/recordings_for_train"
-VAL_AUDIO = "/home/jru34/Ashborer/Audio_Files/recordings_for_test"
-'''
-train_dataset = borer_data(TRAIN_ANNOTATION,TRAIN_AUDIO, device = device)
-val_dataset = borer_data(VAL_ANNOTATION, VAL_AUDIO, device = device)
-
-
-
-#%%
-class CNNNetwork(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-        self.conv1=nn.Sequential(
-            nn.Conv1d(in_channels=1,out_channels=16,kernel_size=3,stride=1,padding=2),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2)
-        )
-        self.conv2=nn.Sequential(
-            nn.Conv1d(in_channels=16,out_channels=32,kernel_size=3,stride=1,padding=2),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2)
-        )
-        self.conv3=nn.Sequential(
-            nn.Conv1d(in_channels=32,out_channels=64,kernel_size=3,stride=1,padding=2),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2)
-        )
-        self.conv4=nn.Sequential(
-            nn.Conv1d(in_channels=64,out_channels=128,kernel_size=3,stride=1,padding=2),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2)
-        )
-        self.conv5=nn.Sequential(
-            nn.Conv1d(in_channels=128,out_channels=256,kernel_size=3,stride=1,padding=2),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2)
-        )
-        self.flatten=nn.Flatten()
-        self.linear1=nn.Linear(in_features=4096,out_features=64)
-        self.linear2=nn.Linear(in_features=64,out_features=1)
-        self.output=nn.Sigmoid()
+sys.path.insert(1, r"C:\Users\jeffu\Documents\Ash Borer Project\pre_processing")
+from custom_dataset_class_96k import borer_data
+from early_stopping import EarlyStopping
+from CNN_Model_1D import CNNNetwork
     
-    def forward(self,input_data):
-        x=self.conv1(input_data)
-        x=self.conv2(x)
-        x=self.conv3(x)
-        x=self.conv4(x)
-        x=self.conv5(x)
-        x=self.flatten(x)
-        x=self.linear1(x)
-        logits=self.linear2(x)
-        output=self.output(logits)
-        
-        return output
-      
-
-
-class EarlyStopping:
-    """Early stops the training if validation loss doesn't improve after a given patience."""
-    def __init__(self, patience=7, verbose=False, delta=0, path='1DAshBorercheckpoint.pt', trace_func=print):
-        """
-        Args:
-            patience (int): How long to wait after last time validation loss improved.
-                            Default: 7
-            verbose (bool): If True, prints a message for each validation loss improvement. 
-                            Default: False
-            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
-                            Default: 0
-            path (str): Path for the checkpoint to be saved to.
-                            Default: 'checkpoint.pt'
-            trace_func (function): trace print function.
-                            Default: print            
-        """
-        self.patience = patience
-        self.verbose = verbose
-        self.counter = 0
-        self.best_score = None
-        self.early_stop = False
-        self.val_loss_min = np.Inf
-        self.delta = delta
-        self.path = path
-        self.trace_func = trace_func
-    def __call__(self, val_loss, model):
-
-        score = -val_loss
-
-        if self.best_score is None:
-            self.best_score = score
-            self.save_checkpoint(val_loss, model)
-        elif score < self.best_score + self.delta:
-            self.counter += 1
-            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
-            if self.counter >= self.patience:
-                self.early_stop = True
-        else:
-            self.best_score = score
-            self.save_checkpoint(val_loss, model)
-            self.counter = 0
-
-    def save_checkpoint(self, val_loss, model):
-        '''Saves model when validation loss decrease.'''
-        if self.verbose:
-            self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.state_dict(), self.path)
-        self.val_loss_min = val_loss
-        
-def train_model(mod,num_epochs):
+def train_model(mod,num_epochs, eps, device, train_loader, val_loader):
     start =time()
     print('Starting Training \n ----------------------------------------')
     model = mod
@@ -149,7 +29,7 @@ def train_model(mod,num_epochs):
     avg_train_acc = []
     avg_test_acc = []
     
-    early_stopping = EarlyStopping(patience=5, verbose=True)
+    early_stopping = EarlyStopping(patience=3, verbose=True)
     for epoch in range(num_epochs):
         if epoch % 10 == 9 or epoch == num_epochs-1 or epoch == 0:
                 print(f'Epoch {epoch+1} \n---------------------')
@@ -157,10 +37,12 @@ def train_model(mod,num_epochs):
         train_loss=[]
         train_acc=[]
         for j,(inputs, labels) in enumerate(train_loader):
+            inputs, labels = inputs.to(device), labels.to(device)
+            smoothed_labels = labels.clamp(eps/2, 1-eps/2).reshape(-1,1).float()
             y_pred = model(inputs).reshape(-1,1).float()
             guess = (y_pred>0.5)*1
             labels = labels.reshape(-1,1).float()
-            loss = loss_fn(y_pred, labels)
+            loss = loss_fn(y_pred, smoothed_labels)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -172,7 +54,6 @@ def train_model(mod,num_epochs):
                 train_acc.append(accuracy_score(y_true=labels.cpu(),y_pred=guess.cpu()))
         avg_train_loss.append(np.average(train_loss))
         avg_train_acc.append(np.average(train_acc))
-    #if epoch % 5 == 0:
         model.eval()
         with torch.no_grad():
             test_loss = []
@@ -199,57 +80,58 @@ def train_model(mod,num_epochs):
        
 
 
-#%%
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device:', device)
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('Using device:', device)
+
+    TRAIN_ANNOTATION = r"C:\Users\jeffu\Documents\Recordings\new_training_data.csv"
+    VAL_ANNOTATION = r"C:\Users\jeffu\Documents\Recordings\new_test_data.csv"
+
+    TRAIN_AUDIO = r"C:\Users\jeffu\Documents\Recordings\new_training"
+    VAL_AUDIO = r"C:\Users\jeffu\Documents\Recordings\new_test"
+    '''
+    TRAIN_ANNOTATION = "/home/jru34/Ashborer/Datasets/training_data_info.csv"
+    VAL_ANNOTATION = "/home/jru34/Ashborer/Datasets/val_data_info.csv"
+
+    TRAIN_AUDIO = "/home/jru34/Ashborer/Audio_Files/recordings_for_train"
+    VAL_AUDIO = "/home/jru34/Ashborer/Audio_Files/recordings_for_test"
+    '''
+    train_dataset = borer_data(TRAIN_ANNOTATION,TRAIN_AUDIO, device = device)
+    val_dataset = borer_data(VAL_ANNOTATION, VAL_AUDIO, device = device)
+
+    overallloss=[]
+    overallacc=[]
+    model = CNNNetwork()
+    loss_fn = nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(),lr =.0003, weight_decay = 0.0)
 
 
 
-overallloss=[]
-overallacc=[]
-model = CNNNetwork()
-loss_fn = nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(),lr =.0003, weight_decay = 0.0)
-
-
-
-train_loader = DataLoader(dataset=train_dataset,
-                        batch_size=64,
-                        shuffle=True,
-                        num_workers=0,
-                        )
-val_loader = DataLoader(dataset = val_dataset,
-                        batch_size=64,
-                        shuffle = True,
-                        num_workers = 0)
-#Can change if using gpu for parallel computing)
-#Training Loop (Very, Very slow, so only do 100 epochs until using HPC)
-
-
-
-model, train_loss, test_loss, train_r2, test_r2 = train_model(model,1000)
-
-
-
-# %%
-with torch.no_grad():
-    fig,axs = plt.subplots(2,2)
-
-    axs[0][0].plot(train_loss, label= 'Training Loss')
-    axs[0][0].legend()
-    axs[0][1].plot(test_loss, 'r', label = 'Test Loss'  )
-    axs[0][1].legend()
-
-
-    axs[1][0].plot(train_r2, label = 'Train Acc')
-    axs[1][0].legend()
-    axs[1][1].plot(test_r2, 'r', label = 'Test Acc')
-    axs[1][1].legend()
-
+    train_loader = DataLoader(dataset=train_dataset,
+                            batch_size=256,
+                            shuffle=True,
+                            num_workers=4,
+                            )
+    val_loader = DataLoader(dataset = val_dataset,
+                            batch_size=256,
+                            shuffle = True,
+                            num_workers = 4)
     
-    #fig.savefig( "/home/jru34/Ashborer/outputs/1D_training_curves.png")
-'''
-    fig.savefig('1Layer.png')
-    overallloss.append(np.mean(overallloss))
-    overallacc.append(np.mean(overallacc))'''
+    model, train_loss, test_loss, train_r2, test_r2 = train_model(model,1000, 0.1, device, train_loader, val_loader)
+    with torch.no_grad():
+        fig,axs = plt.subplots(2,2)
+
+        axs[0][0].plot(train_loss, label= 'Training Loss')
+        axs[0][0].legend()
+        axs[0][1].plot(test_loss, 'r', label = 'Test Loss'  )
+        axs[0][1].legend()
+
+
+        axs[1][0].plot(train_r2, label = 'Train Acc')
+        axs[1][0].legend()
+        axs[1][1].plot(test_r2, 'r', label = 'Test Acc')
+        axs[1][1].legend()        
+        fig.savefig( "/home/jru34/Ashborer/outputs/1D_training_curves.png")
+
+
 # %%
