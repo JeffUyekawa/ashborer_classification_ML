aad978f371a7e1215eaa1dccd3e9932952cb7126
Jeff Uyekawa <jeffuyekawa@gmail.com>
2024-11-18 14:43:21 -0700

    introduced sampling

--- time_series_classification/analyze_tsc_results.py
+++ time_series_classification/analyze_tsc_results.py
@@ -5,16 +5,23 @@ import pandas as pd
 from aeon.classification.convolution_based import RocketClassifier
 import time
 
+def filter_labeled_data(in_path):
+    df = pd.read_csv(in_path)
+    filtered_df = df.drop_duplicates(subset=['File','Start'], keep = 'first')
+    path = r"C:\Users\jeffu\Documents\Recordings\temp_path.csv"
+    filtered_df.to_csv(path, index = False)
+    return path
+
 #Make predictions on test set
-if __name__ == '__main__':
-    data = np.load(r"C:\Users\jeffu\Documents\Ash Borer Project\time_series_classification\filtered_train_test_arrays.npz")
-    X_train = data['X_train']
-    X_test = data['X_test']
-    y_train = data['y_train']
-    y_test = data['y_test']
-    clf = RocketClassifier(num_kernels=500, random_state=13)
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
+
+data = np.load(r"C:\Users\jeffu\Documents\Ash Borer Project\time_series_classification\filtered_train_test_arrays.npz")
+X_train = data['X_train']
+X_test = data['X_test']
+y_train = data['y_train']
+y_test = data['y_test']
+clf = RocketClassifier(num_kernels=500, random_state=13)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
 
 
 # %%
@@ -27,10 +34,11 @@ import matplotlib.pyplot as plt
 data = np.load(r"C:\Users\jeffu\Documents\Ash Borer Project\time_series_classification\cross_val_arrays.npz")
 X_cv = data['X_test']
 y_cv = data['y_test']
+y_pred = clf.predict(X_cv)
 
-
+#%%
 # Compute the confusion matrix
-cm = confusion_matrix(y_test, y_pred)
+cm = confusion_matrix(y_cv, y_pred)
 
 # Use ConfusionMatrixDisplay to visualize the confusion matrix
 disp = ConfusionMatrixDisplay(confusion_matrix=cm)
@@ -49,14 +57,17 @@ FN = cm[1,0]
 acc = (TP + TN)/(TP + TN + FP + FN)
 prec = (TP)/(TP + FP)
 recall = (TP)/(TP + FN)
-f1 = f1_score(y_train,y_pred)
+f1 = f1_score(y_cv,y_pred)
 print(f'Model Accuracy: {acc} | Precision: {prec} | Recall: {recall} | F1: {f1}')
 #%%
 #Need to figure out a way to graph these next
-
 from IPython.display import Audio, display, clear_output
-AUDIO_PATH = r"C:\Users\jeffu\Documents\Recordings\test_set"
-AUDIO_LABELS = r"C:\Users\jeffu\Documents\Recordings\test_set_labels.csv"
+import os
+import torchaudio as ta
+AUDIO_PATH = r"C:\Users\jeffu\Documents\Recordings\new_test"
+AUDIO_LABELS =filter_labeled_data(r"C:\Users\jeffu\Documents\Recordings\new_test_data.csv")
+df = pd.read_csv(AUDIO_LABELS)
+df['Prediction'] = y_pred
 grouped = df.groupby('File')
 
 for i, (file, group) in enumerate(grouped):
