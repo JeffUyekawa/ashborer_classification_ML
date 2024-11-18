149ef0661f3296580b5206046bae30fb0ecfba12
Jeff Uyekawa <jeffuyekawa@gmail.com>
2024-11-18 14:43:42 -0700

    introduced sampling

--- time_series_classification/test_audio_models.py
+++ time_series_classification/test_audio_models.py
@@ -58,57 +58,56 @@ def evaluate_models(X_train, y_train, X_test, y_test):
     #y_train = data['y_train'][:100]
     #y_test = data['y_test'][:100]
     
-
-    #classifiers = {"DTW":KNeighborsTimeSeriesClassifier(distance = 'dtw'),"Rocket": RocketClassifier(num_kernels=200), "HEC":HIVECOTEV2()}
+    classifiers = {"DTW":KNeighborsTimeSeriesClassifier(distance = 'dtw'),"Rocket": RocketClassifier(), "HEC":HIVECOTEV2()}
     
-    kernels = []
+    models = []
     accuracies = []
+    precisions = []
+    recalls = []
+    f1s = []
     train_times = []
     pred_times = []
-    f1s = []
-    precs = []
-    recs = []
-    models = []
-    rockets = []
 
-    num_kernels = [100, 500, 5000, 10000]
-    transforms = ["rocket", "minirocket", "multirocket"]
-    estimators = {"RidgeCV":None, "Logistic Regression":LogisticRegression(max_iter=500)}
-    for trans in transforms:
-        for estimator in estimators:
-            for n in num_kernels:
-                est = estimators[estimator]
-                clf = RocketClassifier(num_kernels=n, rocket_transform=trans,   random_state = 13, estimator= est)
-                print(f'{trans} - {estimator} - {n} kernels \n---------------------------------')
-                acc, f1, prec, rec, train_time, pred_time = train_benchmark(clf, X_train, X_test, y_train, y_test)
-                accuracies.append(acc)
-                train_times.append(train_time)
-                pred_times.append(pred_time)
-                kernels.append(n)
-                f1s.append(f1)
-                precs. append(prec)
-                recs.append(rec)
-                rockets.append(trans)
-                models.append(estimator)
-                del clf
-        
+    for model_name in classifiers:
+        clf = classifiers[model_name]
+        acc, prec, rec, f1, train_time, pred_time = train_benchmark(clf, X_train, X_test, y_train, y_test)
+        accuracies.append(acc)
+        precisions.append(prec)
+        recalls.append(rec)
+        f1s.append(f1)
+        train_times.append(train_time)
+        pred_times.append(pred_time)
+        models.append(model_name)
        
-    results = {"Model": rockets, "Estimator": models,"Kernels": kernels, "Accuracy": accuracies, "F1": f1s, "Precision": precs, "Recall": recs, "Train Time": train_times, "Prediction Time": pred_times}
+    results = {"Model": models, "Accuracy": accuracies, "Precision": precisions, "Recall": recalls, "F1 Score":f1s, "Train Time": train_times, "Prediction Time": pred_times}
     df = pd.DataFrame(results)
     return df
 
+def sample_x_y(X,y, n, m, seed):
+    indices_0 = np.where(y == 0)[0]
+    indices_1 = np.where(y == 1)[0]
+
+    # Randomly sample 50 indices from each class
+    np.random.seed(seed)  # For reproducibility
+    sample_indices_0 = np.random.choice(indices_0, size=n, replace=False)
+    sample_indices_1 = np.random.choice(indices_1, size=m, replace=False)
+
+    # Get the corresponding samples from X_train and y_train
+    X_sampled = np.concatenate([X[sample_indices_0], X[sample_indices_1]])
+    y_sampled = np.concatenate([y[sample_indices_0], y[sample_indices_1]])   
+    return X_sampled, y_sampled
 # %%
 data = np.load(r"C:\Users\jeffu\Documents\Ash Borer Project\time_series_classification\filtered_train_test_arrays.npz")
 X_train = data['X_train']
 X_test = data['X_test']
 y_train = data['y_train']
 y_test = data['y_test']
+
+X_train, y_train = sample_x_y(X_train, y_train, 20, 20)
+X_test, y_test = sample_x_y(X_test, y_test, 10, 10)
 # %%
 df = evaluate_models(X_train, y_train, X_test, y_test)
 
 # %%
-df.to_csv(r"C:\Users\jeffu\Documents\Ash Borer Project\time_series_classification\rocket_results.csv", index = False)
-# %%
-clf = RocketClassifier(num_kernels = 5000, estimator = LogisticRegression(max_iter=2000))
-acc, f1, prec, rec, train_time, pred_time = train_benchmark(clf, X_train, X_test, y_train, y_test)
+df.to_csv(r"C:\Users\jeffu\Documents\Ash Borer Project\time_series_classification\timeseries_results.csv", index = False)
 # %%
