9951a09412e87bb952b4e11091eaa3e5b68272a6
Jeff Uyekawa <jeffuyekawa@gmail.com>
2024-12-16 16:15:02 -0700

    added hyperparameter tuning script for aeon models

---
+++ time_series_classification/hyper_parameter_tuning.py
@@ -0,0 +1,109 @@
+# %%
+import numpy as np
+import pandas as pd
+from aeon.classification.distance_based import KNeighborsTimeSeriesClassifier, ProximityForest
+from aeon.classification.convolution_based import RocketClassifier
+from aeon.classification.feature_based import FreshPRINCEClassifier
+from aeon.classification.hybrid import HIVECOTEV2
+from aeon.classification.deep_learning import InceptionTimeClassifier
+from aeon.classification.dictionary_based import WEASEL
+from aeon.classification.interval_based import RSTSF
+from aeon.classification.shapelet_based import RDSTClassifier
+from sklearn.model_selection import KFold
+import argparse
+
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from time import time 
+import os
+
+
+
+def train_benchmark(classifier, X_train, X_test, y_train, y_test):
+    clf = classifier
+    start = time()
+    print('Starting Training')
+    clf.fit(X_train,y_train)
+    print('Training Complete')
+    end= time()
+    train_time = end-start
+    start = time()
+    print('Making Prediction')
+    y_pred = clf.predict(X_test)
+    end = time()
+    pred_time = end-start
+    print('Calculating Accuracy')
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    print(f"Accuracy: {acc: .2f}\n \
+        Training time: {train_time:.2f} seconds\n \
+        Prediction Time: {pred_time:.2f} seconds")
+    '''
+    Eventually add some code to save model paramters here
+    '''
+    
+    
+    return acc, prec, rec, f1, train_time, pred_time
+
+
+
+
+
+def evaluate_models(X,y, model_name):
+    
+    classifiers = {"DTW":KNeighborsTimeSeriesClassifier(distance = 'dtw'),"Rocket": RocketClassifier(num_kernels=500, random_state=13), "HEC":HIVECOTEV2(), "InceptionTime": InceptionTimeClassifier(), "RDST": RDSTClassifier(), "Weasel": WEASEL(), "RSTSF": RSTSF(), "FreshPRINCE": FreshPRINCEClassifier(), "PF": ProximityForest()}
+    
+    models = []
+    accuracies = []
+    precisions = []
+    recalls = []
+    f1s = []
+    train_times = []
+    pred_times = []
+    folds = []
+    
+
+    CV = KFold(n_splits = 5, shuffle = True, random_state = 13)
+    for i , (train_idx, test_idx) in enumerate(CV.split(X)):
+        X_train = X[train_idx]
+        X_test = X[test_idx]
+        y_train = y[train_idx]
+        y_test = y[test_idx]
+        clf = classifiers[model_name]
+        acc, prec, rec, f1, train_time, pred_time = train_benchmark(clf, X_train, X_test, y_train, y_test)
+        accuracies.append(acc)
+        precisions.append(prec)
+        recalls.append(rec)
+        f1s.append(f1)
+        train_times.append(train_time)
+        pred_times.append(pred_time)
+        models.append(model_name)
+        folds.append(i+1)
+    folds.append("Average")
+    models.append(model_name)
+    for metric in [accuracies, precisions, recalls, f1s, train_times, pred_times]:
+        metric.append(np.average(metric))
+    
+
+       
+    results = {"Model": models,"Fold": folds, "Accuracy": accuracies, "Precision": precisions, "Recall": recalls, "F1": f1s, "Train Time": train_times, "Prediction Time": pred_times}
+    df = pd.DataFrame(results)
+    return df
+
+
+
+# %%
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate a time series classification model.")
+    parser.add_argument("--model_name", type=str, required=True, help="Name of the model to evaluate.")
+    args = parser.parse_args()
+    data = np.load('/scratch/jru34/minimal_train_test_arrays.npz')
+    X = data['X_test']
+    y = data['y_test']
+    del data
+    model_name = args.model_name
+    df = evaluate_models(X,y, model_name)
+    save_path = os.path.join(PARENT_PATH,f'{model_name}_results.csv')
+    df.to_csv(save_path, index = False)
+    
